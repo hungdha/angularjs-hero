617870c898dab5d9eb8bbd25484b56c75e312b1c
hungdha <tuanhung.cntt@gmail.com>
2019-04-06 13:43:18 +0700

    build to post heroku

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man', skills : ['vu le', 'chuong'] },
+  { id: 2, name: 'Bat Man' , skills: ['fly']},
+  { id: 3, name: 'Super Man', skills : ['fly'] },
+  { id: 4, name: 'Joker', skills : ['flight'] },
+  { id: 5, name: 'Spider Man', skills : ['fly'] },
+  { id: 6, name: 'Wonder Woman', skills : ['fly'] },
+  { id: 7, name: 'Hulk', skills : ['strong'] },
+  { id: 8, name: 'Captain America', skills : ['strong'] },
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
